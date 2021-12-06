57cdec1149125fedd3f62b389d48a30938cc8e17
guillemnobi <guillemguillem01@gmail.com>
2021-12-06 22:32:01 +0100

    changes

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,7 +1,6 @@
-import React from "react";
 import { Bar } from "react-chartjs-2";
 
-const Chart = () => {
+const Chart = ({ chartData }) => {
   return (
     <div className="chart">
       <Bar data={{ labels: ["red", "blue"] }} height={400} width={600} />
