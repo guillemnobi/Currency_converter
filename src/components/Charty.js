ff9762382816b79824330bd93908851f66b0447a
guillemnobi <guillemguillem01@gmail.com>
2021-12-07 16:30:53 +0100

    changes 07012

---
+++ src/components/Charty.js
@@ -0,0 +1,11 @@
+import Chart from "chart.js/auto";
+
+const Charty = () => {
+  return (
+    <div className="chart">
+      <canvas id="chart-canvas" width="600" height="350" />
+    </div>
+  );
+};
+
+export default Charty;
--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Chart from "./Chart";
+// import Chart from "./Chart";
+import Chart from "chart.js/auto";
+import Charty from "./Charty";
 
 const List = ({ baseCurrency }) => {
   const [list, setList] = useState(false);
   const [date, setDate] = useState("");
   const [currencyB, setCurrencyB] = useState(null);
-  const [chartData, setChartData] = useState();
 
   useEffect(() => {
     axios
@@ -21,24 +22,59 @@ const List = ({ baseCurrency }) => {
       .catch((error) => console.log(error));
   }, [baseCurrency]);
 
+  let chart;
+
+  const buildChart = (labels, data, label) => {
+    // const chartRef = document.getElementById('chart-canvas').getContext("2d");
+    if (typeof chart !== "undefined") {
+      chart.destroy();
+    }
+
+    chart = new Chart(
+      document.getElementById("chart-canvas").getContext("2d"),
+      {
+        type: "line",
+        data: {
+          labels,
+          datasets: [
+            {
+              label: label,
+              data,
+              borderColor: "#8e5ea2",
+              fill: false,
+              tension: 0,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+        },
+      }
+    );
+  };
+
   useEffect(() => {
     axios
       .get(
         `https://altexchangerateapi.herokuapp.com/2021-01-01..${date}?from=${baseCurrency}&to=${currencyB}`
       )
       .then((res) => {
-        setChartData({
-          chartX: Object.keys(res.data.rates),
-          chartY: Object.values(res.data.rates).map((rate) => rate[currencyB]),
-        });
+        const chartLabels = Object.keys(res.data.rates);
+        const chartData = Object.values(res.data.rates).map(
+          (rate) => rate[currencyB]
+        );
+        const chartLabel = `${baseCurrency}/${currencyB}`;
+        buildChart(chartLabels, chartData, chartLabel);
       })
       .catch((error) => console.log(error));
   }, [currencyB]);
 
-  console.log(chartData);
-
   const handleClick = (e) => {
     setCurrencyB(e.target.textContent);
+  };
+
+  const handleDestroy = () => {
+    console.log(chart);
   };
 
   return (
@@ -68,8 +104,9 @@ const List = ({ baseCurrency }) => {
           </table>
           <p id="date">* Updated at {date}</p>
         </div>
-        {currencyB !== null && <Chart />}
+        {currencyB !== null && <Charty />}
       </div>
+      <button onClick={handleDestroy}>Destroy</button>
     </>
   );
 };
