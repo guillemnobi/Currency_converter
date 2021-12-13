33f22212b165148a5a284e9eaa651d51606b46fb
guillemnobi <guillemguillem01@gmail.com>
2021-12-14 00:18:29 +0100

    changes 1412

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import AOS from "aos";
 function App() {
   AOS.init();
   return (
-    <div className="general">
-      <Navbar />
-    </div>
+    <>
+      <div className="general">
+        <Navbar />
+      </div>
+    </>
   );
 }
 
